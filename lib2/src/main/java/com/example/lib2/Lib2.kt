f7da25c18fcf5d7b081cd66c6cc7ef9bb0977b52
avdim <avdim@mail.ru>
2020-08-24 12:25:59 +0300

    update lib

--- lib2/src/main/java/com/example/lib2/Lib2.kt
+++ lib2/src/main/java/com/example/lib2/Lib2.kt
@@ -1,5 +1,5 @@
 package com.example.lib2
 
-import com.example.lib1.LIB_1_TEXT
+import com.example.lib1.LIB_1_TEXT_CHANGED
 
-val LIB_2_TEXT = "lib_2 use $LIB_1_TEXT"
+val LIB_2_TEXT = "lib_2 use $LIB_1_TEXT_CHANGED"
